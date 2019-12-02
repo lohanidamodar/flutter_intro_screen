adb2b3763c3a51b387d60fe84362b0f9affd2dd8
Damodar Lohani <dlohani48@gmail.com>
2019-12-02 10:31:08 +0545

    intro screen final

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 void main() => runApp(MyApp());
 
@@ -20,92 +21,141 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: IntroScreen(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class IntroScreen extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _IntroScreenState createState() => _IntroScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+class _IntroScreenState extends State<IntroScreen> {
+  SwiperController _controller = SwiperController();
+  int _currentIndex = 0;
+  final List<String> titles = [
+    "Welcome",
+    "Awesome App",
+    "Flutter App",
+  ];
+  final List<String> subtitles = [
+    "Welcome to this awesome intro screen app.",
+    "This is an awesome app, of intro screen design",
+    "Flutter is awesome for app development"
+  ];
+  final List<Color> colors = [
+    Colors.green.shade300,
+    Colors.blue.shade300,
+    Colors.indigo.shade300,
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Swiper(
+            loop: false,
+            index: _currentIndex,
+            onIndexChanged: (index) {
+              setState(() {
+                _currentIndex = index;
+              });
+            },
+            controller: _controller,
+            pagination: SwiperPagination(
+              builder: DotSwiperPaginationBuilder(
+                activeColor: Colors.red,
+                activeSize: 20.0,
+              ),
+            ),
+            itemCount: 3,
+            itemBuilder: (context, index) {
+              return IntroItem(
+                title: titles[index],
+                subtitle: subtitles[index],
+                bg: colors[index],
+                imageUrl: "assets/${index + 1}.png",
+              );
+            },
+          ),
+          Align(
+            alignment: Alignment.bottomLeft,
+            child: FlatButton(
+              child: Text("Skip"),
+              onPressed: () {},
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: IconButton(
+              icon:
+                  Icon(_currentIndex == 2 ? Icons.check : Icons.arrow_forward),
+              onPressed: () {
+                if (_currentIndex != 2) _controller.next();
+              },
+            ),
+          )
+        ],
+      ),
+    );
   }
+}
+
+class IntroItem extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final Color bg;
+  final String imageUrl;
+
+  const IntroItem(
+      {Key key, @required this.title, this.subtitle, this.bg, this.imageUrl})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+    return Container(
+      color: bg ?? Theme.of(context).primaryColor,
+      child: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: <Widget>[
+              const SizedBox(height: 40),
+              Text(
+                title,
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 35.0,
+                    color: Colors.white),
+              ),
+              if (subtitle != null) ...[
+                const SizedBox(height: 20.0),
+                Text(
+                  subtitle,
+                  style: TextStyle(color: Colors.white, fontSize: 24.0),
+                  textAlign: TextAlign.center,
+                ),
+              ],
+              const SizedBox(height: 40.0),
+              Expanded(
+                child: Container(
+                  margin: const EdgeInsets.only(bottom: 70),
+                  width: double.infinity,
+                  child: ClipRRect(
+                      borderRadius: BorderRadius.circular(20.0),
+                      child: Material(
+                        elevation: 4.0,
+                          child: Image.asset(
+                        imageUrl,
+                        fit: BoxFit.cover,
+                      ),),),
+                ),
+              )
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
